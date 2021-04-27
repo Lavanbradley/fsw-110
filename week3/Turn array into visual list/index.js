cd5eb449c98c7204cf0363fd161c09ed33af9ae9
Lavanbradley <lavan.bradley@bryanuniversity.edu>
2021-04-26 20:37:26 -0700

    Made changes

--- week3/Turn array into visual list/index.js
+++ week3/Turn array into visual list/index.js
@@ -1,5 +1,3 @@
-
-
 for (var i = 0; i < 10; i++) {
   var h1 = document.createElement('h1');
   h1.textContent = 'Hello world';
@@ -7,10 +5,12 @@ for (var i = 0; i < 10; i++) {
   document.body.appendChild(h1);
 };
 
+
+
 var names = ['steve', 'larry', 'joe', 'shirley', 'steph', 'nate', 'rick', 'emily'];
 
-
-var nameList = document.getElementById('name');
 for (var i = 0; i < names.length; i++) {
-  nameList.innerHTML += '<li>' + names[i] + '</li>'
-}
+  var ul = document.createElement('ul');
+  document.body.appendChild(ul);
+  ul.innerHTML += '<li>' + names[i] + '</li>'
+};
